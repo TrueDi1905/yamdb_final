2ccea78d767060f38171b737b659650118dd9c5c
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-09-02 16:17:18 +0300

    '+'

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
-import environ
-env = environ.Env()
-environ.Env.read_env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
